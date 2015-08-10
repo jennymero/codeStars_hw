66b22b79bab4aad9662210c6efaedbe39710b455
jennymero <jenny@jennymero.com>
2015-08-09 23:35:05 -0400

    added shape class

---
+++ hw2_question3_shapeClass/shapeClass/Shape.m
@@ -0,0 +1,28 @@
+//
+//  shape.m
+//  shapeClass
+//
+//  Created by JENNY MERO on 8/4/15.
+//  Copyright (c) 2015 JENNY MERO. All rights reserved.
+//
+
+#import "Shape.h"
+
+@implementation Shape
+
+
+//Methods
+
+
++(int) calculateArea{
+    return self.totalArea;
+}
+
+-(int) totalArea{
+    return self.width * self.height;
+}
+
+
+
+
+@end
